a445d50f272e68fbfa52b90a53447d6ef13fcbd7
kimyonghwa <codej99@nhnent.com>
2019-04-24 19:20:32 +0900

    Spring Oauth2 ResourceServer

---
+++ src/main/java/com/rest/oauth2/config/Oauth2ResourceServerConfig.java
@@ -0,0 +1,10 @@
+package com.rest.oauth2.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+}
