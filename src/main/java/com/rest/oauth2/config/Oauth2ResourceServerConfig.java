c31eeaa5463ef70df1f1b75b41c1a38a8d0b0f9f
kimyonghwa <codej99@nhnent.com>
2019-04-26 10:31:31 +0900

    Spring Boot Oauth2 - ResourceServer
    - update settings jwt

--- src/main/java/com/rest/oauth2/config/Oauth2ResourceServerConfig.java
+++ src/main/java/com/rest/oauth2/config/Oauth2ResourceServerConfig.java
@@ -1,13 +1,21 @@
 package com.rest.oauth2.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableResourceServer
 public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${security.oauth2.jwt.signkey}")
+    private String signKey;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -15,5 +23,17 @@ public class Oauth2ResourceServerConfig extends ResourceServerConfigurerAdapter 
         http.authorizeRequests()
                 .antMatchers("/v1/users").access("#oauth2.hasScope('read')")
                 .anyRequest().authenticated();
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signKey);
+        return converter;
     }
 }
